929b49a8cd77fc52215f78457c8caeb987308339
divyap7 <divyaprem25@gmail.com>
2018-06-24 01:51:47 +0530

    #9 Completed seekbar layout activity

---
+++ app/src/main/java/com/playground/android/SeekBarActivity.java
@@ -0,0 +1,34 @@
+package com.playground.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class SeekBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seek_bar);
+        final TextView textViewSeekBarVal = findViewById(R.id.seekBarValue);
+        SeekBar seekBar = findViewById(R.id.seekBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textViewSeekBarVal.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}

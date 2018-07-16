cdc532a43d15e1e1c80a76eff46c5b6de97cd6fb
divyap7 <divyaprem25@gmail.com>
2018-07-17 00:01:17 +0530

    #14 Completed the task

--- app/src/main/java/com/playground/android/Activity_progressBar_Button.java
+++ app/src/main/java/com/playground/android/Activity_progressBar_Button.java
@@ -1,5 +1,6 @@
 package com.playground.android;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,25 +8,68 @@ import android.view.View;
 
 public class Activity_progressBar_Button extends AppCompatActivity {
 
+    View progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress_bar__button);
 
         View button = findViewById(R.id.calculate);
-        View progressBar = findViewById(R.id.progressBar2);
+        progressBar = findViewById(R.id.progressBar2);
+        progressBar.setVisibility(View.INVISIBLE);
 
-        progressBar.setVisibility(View.VISIBLE);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                long sum = 0;
-                for (long i = 1; i <= 999999999; i++) {
-                    sum += i;
-                }
-                Log.d("sum", "sum: " + sum);
+
+                new LongOperation().execute();
+
+
             }
         });
+    }
+
+    private class LongOperation extends AsyncTask<Void, Long, Long> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.INVISIBLE);
+            Log.d("message", "inside pre execute");
+        }
+
+        @Override
+        protected Long doInBackground(Void... voids) {
+            long sum = 0;
+
+            for (long i = 1; i <= 999999999; i++) {
+                Log.d("message", "inside loop");
+                sum += i;
+                publishProgress(sum);
+
+            }
+            Log.d("sum","Sum is "+sum);
+
+            return sum;
+        }
+
+        @Override
+        protected void onProgressUpdate(Long... values) {
+            super.onProgressUpdate(values);
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(Long aLong) {
+            super.onPostExecute(aLong);
+            progressBar.setVisibility(View.INVISIBLE);
+            Log.d("message", "inside post execute");
+
+        }
+
+
+
     }
 }
